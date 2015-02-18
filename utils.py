964dca6f32c00a4334c15500dfc262793b1b927d
Adam McCarthy <aem1269@rit.edu>
2015-02-18 17:23:40 -0500

    Add absolute utils

--- utils.py
+++ utils.py
@@ -12,6 +12,36 @@ def load_common_words(filename):
     return words
 
 """
+Determine if a url is an absolute URL (it contains a protocol specifier)
+Params:
+    url: The url to check for absoluteness
+Returns:
+    true if the URL is absolute, false otherwise
+"""
+def is_absolute(url):
+    return "://" in url
+
+"""
+Converts a URL to absolute from if it is not already in absolute form.
+Params:
+    base: the base domain
+    source: the URL we came from
+    url: The URL to convert to an absolute URL
+Returns:
+    An absolute URL referring to the given path.
+"""
+def to_absolute(base, source, url):
+    # if URL already absolute, just return it
+    if is_absolute(url):
+        return url
+
+    if url[0] == "/": # e.g. /info/about.html
+        return "http://" + base + url
+
+    else: # e.g. projects/medialist
+        return source + "/" + url
+
+"""
 Parses the domain name from a URL. If no protocol is specified, it is assumed
 that the path is a relative path, and so no domain can be found.
 params:
@@ -62,6 +92,14 @@ returns
 def filter_externals(base, urls):
     filtered_urls = []
     for url in urls:
-        if (domain_matches(base, url)):
+        if domain_matches(base, url):
             filtered_urls.append(url)
     return filtered_urls
+
+
+if __name__ == "__main__":
+    base = "corb.co"
+    source = "http://corb.co/projects"
+    print(is_absolute(source))
+    print(is_absolute("www.google.com"))
+    print(to_absolute(base, source, "/medialist"))
