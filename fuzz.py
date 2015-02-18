b8ab443cb2e3b5e26004a893ff8d3ab4a82da9aa
Adam McCarthy <aem1269@rit.edu>
2015-02-18 15:30:28 -0500

    add argument parsing

--- fuzz.py
+++ fuzz.py
@@ -1,12 +1,47 @@
 from utils import *
 from sys import argv
+from argparse import ArgumentParser
+
+VALID_COMMANDS = ['discover', 'test']
 
 """ The main function """
 def main():
-	# TODO Parsing arguments
-	words = load_common_words("words.txt")
-	extensions = load_common_words("extensions.txt")
-	print(words)
+    # initialize an argument parser
+    parser = ArgumentParser(
+        description="Discover vulnerabilities for web applications"
+    )
+    # define 'help' strings up here to be cleaner
+    command_desc = ("[ discover / test ] Whether to discover available pages" +
+        " or test for vulnerabilities")
+    url_desc = "The URL of the website to discover or test"
+    common_desc = ("(discover only) Text file containing a list of common " +
+        "words to try when searching for pages")
+
+    # add command line arguments to the parser
+    parser.add_argument('command', metavar='command', type=str,
+        help=command_desc)
+    parser.add_argument('url', metavar='url', type=str,
+        help=url_desc)
+    parser.add_argument('--common-words', metavar='file', help=common_desc)
+
+    # parse the arguments
+    args = parser.parse_args()
+
+    # validate arguments
+    if (args.command.lower() not in VALID_COMMANDS):
+        print("invalid command %s." % args.command)
+        parser.print_help()
+        exit()
+    if (args.command.lower() == "test"):
+        print("test not implemented yet")
+        exit()
+    
+
+    words = []
+    if (args.common_words):
+        words=load_common_words(args.common_words)
+    extensions = load_common_words("extensions.txt")
+    print(words)
 
 if __name__ == "__main__":
-	main()
+    main()
--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
 """
 Reads in each line from a text file and returns a list of the words found.
 params:
-	filename: path to the file to open
+    filename: path to the file to open
 returns:
-	An array-list of words found
+    An array-list of words found
 """
 def load_common_words(filename):
-	words = []
-	for line in open(filename):
-		words.append(line.strip())
-	return words
+    words = []
+    for line in open(filename):
+        words.append(line.strip())
+    return words
