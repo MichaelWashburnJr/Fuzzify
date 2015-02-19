2fc8430643a8c9c83fb74cb5e861d87a56953277
corbanmailloux <cdm3806@rit.edu>
2015-02-18 20:06:57 -0500

    Only maintain valid links.

--- crawl.py
+++ crawl.py
@@ -3,18 +3,22 @@ from bs4 import BeautifulSoup as bs
 from utils import *
 
 global visited
+global good_links
 
 def crawl(domain, url):
     global visited
+    global good_links
     visited = set()
+    good_links = set()
     recurse_crawl(domain, url)
-    sortList = sorted(visited)
-    print("Visited List (sorted):")
-    for url in sortList:
+    
+    print("\n\n\nLink List (sorted):")
+    for url in sorted(good_links):
         print(url);
 
 def recurse_crawl(domain, url):
     global visited
+    global good_links
     print("Crawling: " + url)
     links = set()
     try:
@@ -25,6 +29,7 @@ def recurse_crawl(domain, url):
     if r.status_code != 200:
         print("Status code: " + str(r.status_code))
         return links
+    good_links.add(url)
     beautiful = bs(r.content)
     for link in beautiful.find_all('a'):
         linkContent = link.get('href')
