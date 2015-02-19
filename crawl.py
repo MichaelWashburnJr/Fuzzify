0d2b8744eea4b8c114255a9ca4d3bf6d38b407f2
corbanmailloux <cdm3806@rit.edu>
2015-02-18 21:52:05 -0500

    Added basic input grabbing.

--- crawl.py
+++ crawl.py
@@ -4,21 +4,51 @@ from utils import *
 
 global visited
 global good_links
+global cookies
+global inputs
+
+class InputField:
+    def __init__(self, field_element, url):
+        self.name = field_element.get('name')
+        self.id = field_element.get('id')
+        self.type = field_element.get('type')
+        self.from_url = url
+    def __str__(self):
+        return "InputField { name:%s id:%s type:%s from_url:%s }" % (self.name, self.id, self.type, self.from_url)
+
 
 def crawl(domain, url):
     global visited
     global good_links
+    global cookies
+    global inputs
+
     visited = set()
     good_links = set()
+    cookies = set()
+    inputs = set()
+
     recurse_crawl(domain, url)
     
     print("\n\n\nLink List (sorted):")
     for url in sorted(good_links):
         print(url);
 
+    print("\n\n\nCookie List:")
+    for cookie in cookies:
+        print(cookie);
+
+    print("\n\n\nInput List:")
+    for input_item in inputs:
+        print(input_item);
+
+
 def recurse_crawl(domain, url):
     global visited
     global good_links
+    global cookies
+    global inputs
+
     print("Crawling: " + url)
     links = set()
     try:
@@ -29,8 +59,19 @@ def recurse_crawl(domain, url):
     if r.status_code != 200:
         print("Status code: " + str(r.status_code))
         return links
+
     good_links.add(url)
+    for cookie in r.cookies:
+        if cookie not in cookies:
+            cookies.add(cookie)
+
     beautiful = bs(r.content)
+
+    # Find all inputs
+    for input_field in beautiful.find_all('input'):
+        inputs.add(InputField(input_field, url))
+
+    # Find all links
     for link in beautiful.find_all('a'):
         linkContent = link.get('href')
         if linkContent is not None:
