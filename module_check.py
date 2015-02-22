3d6c3d1d6178baba01742fa25607dd5c2b2ac509
Michael Washburn Jr <mdw7326@rit.edu>
2015-02-21 20:44:51 -0500

    Add requirements.txt and relavent error message

--- module_check.py
+++ module_check.py
@@ -9,10 +9,12 @@ def TryModule(module_name, print_name, pip_name):
         return True
     except ImportError:
         print("Error: Package \"" + print_name +"\" not found.")
-        print("       install with \"pip install " + pip_name +"\"")
         return False
 
 """ Check for required packages"""
 all_found = True;
 all_found &= TryModule("requests", "Requests", "requests")
 all_found &= TryModule("bs4", "BeautifulSoup4", "BeautifulSoup4")#BeautifulSoup4
+
+if not all_found:
+	print("Install missing packages with \"pip install -r requirements.txt\"")
