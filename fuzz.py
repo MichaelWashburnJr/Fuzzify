75570de7bef926190d3ccb50c64c7b4d8ded5b83
Adam McCarthy <aem1269@rit.edu>
2015-03-13 23:01:55 -0400

    Move command line arg parsing into utils.py

--- fuzz.py
+++ fuzz.py
@@ -12,10 +12,9 @@ import module_check
 # Do all imports if the required packages were found
 if module_check.all_found:
     from url import Url
-    from utils import load_lines_from_file
+    from utils import load_lines_from_file, parse_args
     from crawl import crawl
     from sys import argv
-    from argparse import ArgumentParser
 
 VALID_COMMANDS = ['discover', 'test']
 
@@ -24,41 +23,9 @@ The main function.
 Parses command line arguments and call crawl.
 """
 def main():
-    # Initialize an argument parser
-    parser = ArgumentParser(
-        description="Discover vulnerabilities for web applications"
-    )
-    # Define 'help' strings up here to be cleaner
-    command_desc = ("[ discover / test ] Whether to discover available pages"+
-        " or test for vulnerabilities")
-    url_desc = "The URL of the website to discover or test"
-    common_desc = ("(discover only) Text file containing a list of common "+
-        "words to try when searching for pages")
-    auth_desc = "[ dvwa / bodgeit ] The custom authentication method to use"
-    vect_desc = "A file containing common strings used to exploit web pages"
-    sens_desc = ("A file containing data that is determined to be \"sensitive"+
-        "\", i.e. users should not see it")
-    rand_desc = ("When off, try each input to each page systematically."+
-        " When on, choose a random page, then a random input field and test"+
-        " all vectors. Default: false.")
-    slow_desc = ("Number of milliseconds considered when a response is cons"+
-        "idered \"slow\". Default is 500 milliseconds")
-
-
-    # Add command line arguments to the parser
-    parser.add_argument('command', metavar='command', type=str,
-        help=command_desc)
-    parser.add_argument('url', metavar='url', type=str, help=url_desc)
-    parser.add_argument('--common-words', metavar='<file>', help=common_desc, 
-        required=True)
-    parser.add_argument('--custom-auth', metavar='string', help=auth_desc)
-    parser.add_argument('--vectors', metavar='<file>', help=vect_desc)
-    parser.add_argument('--sensitive', metavar='<file>', help=sens_desc)
-    parser.add_argument('--random', metavar='[true/false]', help=rand_desc)
-    parser.add_argument('--slow', metavar='<ms>', help=slow_desc)
 
     # Parse the arguments
-    args = parser.parse_args()
+    args = parse_args()
 
     # Validate arguments
     if (args.command.lower() not in VALID_COMMANDS):
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 """
 util.py
 
@@ -30,3 +31,45 @@ def load_lines_from_file(filename):
     for line in open(filename):
         lines.append(line.strip())
     return lines
+
+"""
+Creates an argument parser for the program. Use it to parse the CL args
+Putting this code here clears up main a bunch.
+Returns:
+    Parsed command line arguments
+"""
+def parse_args():
+        # Initialize an argument parser
+    parser = ArgumentParser(
+        description="Discover vulnerabilities for web applications"
+    )
+    # Define 'help' strings up here to be cleaner
+    command_desc = ("[ discover / test ] Whether to discover available pages"+
+        " or test for vulnerabilities")
+    url_desc = "The URL of the website to discover or test"
+    common_desc = ("(discover only) Text file containing a list of common "+
+        "words to try when searching for pages")
+    auth_desc = "[ dvwa / bodgeit ] The custom authentication method to use"
+    vect_desc = "A file containing common strings used to exploit web pages"
+    sens_desc = ("A file containing data that is determined to be \"sensitive"+
+        "\", i.e. users should not see it")
+    rand_desc = ("When off, try each input to each page systematically."+
+        " When on, choose a random page, then a random input field and test"+
+        " all vectors. Default: false.")
+    slow_desc = ("Number of milliseconds considered when a response is cons"+
+        "idered \"slow\". Default is 500 milliseconds")
+
+
+    # Add command line arguments to the parser
+    parser.add_argument('command', metavar='command', type=str,
+        help=command_desc)
+    parser.add_argument('url', metavar='url', type=str, help=url_desc)
+    parser.add_argument('--common-words', metavar='<file>', help=common_desc, 
+        required=True)
+    parser.add_argument('--custom-auth', metavar='string', help=auth_desc)
+    parser.add_argument('--vectors', metavar='<file>', help=vect_desc)
+    parser.add_argument('--sensitive', metavar='<file>', help=sens_desc)
+    parser.add_argument('--random', metavar='[true/false]', help=rand_desc)
+    parser.add_argument('--slow', metavar='<ms>', help=slow_desc)
+
+    return parser.parse_args()
